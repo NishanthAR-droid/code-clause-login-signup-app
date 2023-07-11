207f2df7c7a2c3486c6bc4534503d904a2bf286e
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2023-07-11 19:32:31 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAP_VtMYZEFcEHT99sDnuae77DjPALndBM",
-  authDomain: "code-clause-login-signup.firebaseapp.com",
-  projectId: "code-clause-login-signup",
-  storageBucket: "code-clause-login-signup.appspot.com",
-  messagingSenderId: "82363083402",
-  appId: "1:82363083402:web:2e7322d2dba8cfbc3c1b73",
-  measurementId: "G-K2H7Y1ZQ2Y",
+  apiKey: "<YOURAPIKEY>",
+  authDomain: "<YOURAUTHDOAMIN>",
+  projectId: "<YOURPROJECTID>",
+  storageBucket: "<YOURSTORAGEBUCKET>",
+  messagingSenderId: "<YOURMESSAGINGSENDERID>",
+  appId: "<YOURAPPID>",
 };
 
 const app = initializeApp(firebaseConfig);
